2ca7d3800b32c9ad497568a67e5a6502cdcc80a4
Si4ov <91410683+Si4ov@users.noreply.github.com>
2023-12-06 13:27:44 +0200

    changes main.cpp
    Reduce code 
    made separete method for BotEvents

--- TelegramBot/main.cpp
+++ TelegramBot/main.cpp
@@ -1,5 +1,5 @@
-#include <tgbot/tgbot.h>
 #include <stdio.h>
+#include <tgbot/tgbot.h>
 #include <vector>
 #include <string>
 #include <future>
@@ -8,18 +8,13 @@
 #include <mutex>
 #include <atomic>
 #include <queue>
-#include <unordered_set>
 #include <thread>
 #include <chrono>
 
 
-using namespace TgBot;
-using namespace std;
-
-vector<string> bot_commands{"start", "find_fibonacci", "subdiv_5"};
 
 
-void CalcFibonacci(long long& Data, long long& Results)
+std::string CalcFibonacci(long long& Data, long long& Results)
 {
 	long long a = 1, b = 1;
 
@@ -30,9 +25,11 @@ void CalcFibonacci(long long& Data, long long& Results)
 	}
 
 	(Results == 1) ? Results = b : Results = a;
+	std::string resultString = { "The " + std::to_string(Data) + " number in Fibonacci sequence is " };
+	return resultString;
 }
 
-void CalcSumDivisibleBy5(long long &Data, long long& Results)
+std::string CalcSumDivisibleBy5(long long &Data, long long& Results)
 {
 
 	int sum = 0;
@@ -42,11 +39,13 @@ void CalcSumDivisibleBy5(long long &Data, long long& Results)
 		};
 	}
 	Results = sum;
+	std::string resultString = { "The summation of all numbers that can be divided by 5 without any remainder is " };
+	return resultString;
 }
 
 // Threadpool
 
-class thread_pool
+class ThreadPool
 {
 	//LIST OF TASKS MUTEX AND COND VAR
 	std::queue<std::pair<std::future<void>, int>> QueueOfTasks;
@@ -54,70 +53,43 @@ class thread_pool
 	std::mutex Queue_mutex;
 	std::condition_variable Queue_ConditionVar;
 
-	//LIST OF COMPLETED TASKS MUTEX AND COND VAR
-	std::unordered_set<int> Completed_Tasks;
-
-	std::condition_variable Completed_Tasks_cv;
-	std::mutex Completed_Tasks_mutex;
-
 	//LIST OF THREADS
-	vector<std::thread> threads;
+	std::vector<std::thread> Threads;
 
 
-	std::atomic<bool> quite{ false };
+	std::atomic<bool> Quite{ false };
 
 	//LAST INDEX
 	std::atomic<int> last_idx = 0;
 
 public:
 	//CREATION LIST OF THREADS AND RUN THEM
-	thread_pool(int num_threads)
+	ThreadPool(int num_threads)
 	{
-		threads.reserve(num_threads);
+		Threads.reserve(num_threads);
 		for (int i = 0; i < num_threads; ++i) {
-			threads.emplace_back(&thread_pool::run, this);
+			threads.emplace_back(&ThreadPool::RunThread, this);
 		}
 	};
 
 
 	//ADD TASK TO Queue
 	template <typename Func, typename... Args>
-	int add_task(const Func& task_func, Args&&... args)
+	void AddTask(const Func& task_func, Args&... args)
 	{
 		int task_idx = last_idx++;
 		std::lock_guard<std::mutex> q_lock(Queue_mutex);
 		QueueOfTasks.emplace(std::async(std::launch::deferred, task_func, args...), task_idx);
 		Queue_ConditionVar.notify_one();
-		return task_idx;
+
 	}
 
-	void wait(int task_id)
-	{
-		std::unique_lock<std::mutex> lock(Completed_Tasks_mutex);
-		Completed_Tasks_cv.wait(lock, [this, task_id]() -> bool {
-			return Completed_Tasks.find(task_id) != Completed_Tasks.end();
-		});
-	}
 
-	void wait_all()
-	{
-		std::unique_lock<std::mutex> lock(Queue_mutex);
-		Completed_Tasks_cv.wait(lock, [this]() -> bool {
-			std::lock_guard<std::mutex> task_lock(Completed_Tasks_mutex);
-			return QueueOfTasks.empty() && last_idx == Completed_Tasks.size();
-		});
-	}
-
-	bool calculated(int task_id)
-	{
-		std::lock_guard<std::mutex> lock(Completed_Tasks_mutex);
-		return Completed_Tasks.find(task_id) != Completed_Tasks.end();
-	}
 
 	//Destruct
-	~thread_pool()
+	~ThreadPool()
 	{
-		quite = true;
+		Quite = true;
 		Queue_ConditionVar.notify_all();
 		for (auto& thread : threads) {
 			thread.join();
@@ -125,9 +97,9 @@ public:
 	}
 
 
-	private:
+private:
 	//RUN THREAD
-	void run()
+	void RunThread()
 	{
 		while (!quite) {
 			std::unique_lock<std::mutex> lock(Queue_mutex);
@@ -135,111 +107,90 @@ public:
 			// wait if queue of tasks is empty
 			Queue_ConditionVar.wait(lock, [this]()->bool { return !QueueOfTasks.empty() || quite; });
 
-			if (!QueueOfTasks.empty()) {
+			if(!QueueOfTasks.empty()) {
 				auto elem = std::move(QueueOfTasks.front());
 				QueueOfTasks.pop();
 
 				elem.first.get();
-
-				std::lock_guard<std::mutex> lock(Completed_Tasks_mutex);
-
-
-				Completed_Tasks.insert(elem.second);
-
-				Completed_Tasks_cv.notify_all();
 			}
 		}
 	};
 
 };
 
+template <typename Callable>
+void BotEvent(TgBot::Bot& DataBot, ThreadPool& poolOfThreadsData, Callable& CalculatedMethod , int range, bool& isProcessing)
+{
+	DataBot.getEvents().onNonCommandMessage([&DataBot, &poolOfThreadsData, &range, CalculatedMethod, &isProcessing](TgBot::Message::Ptr message) {
+		std::this_thread::sleep_for(std::chrono::seconds(5));
+		if (isProcessing)
+		{
+			DataBot.getApi().sendMessage(message->chat->id, "Range = " + std::to_string(range));
+			long long number = std::stoi(message->text);
+			DataBot.getApi().sendMessage(message->chat->id, "Calculate...");
+			if (number > 0 && number <= range) {
+				long long results = 0;
+
+				poolOfThreadsData.AddTask(CalculatedMethod, number, results);
+
+				DataBot.getApi().sendMessage(message->chat->id, "Result: " + std::to_string(results));
+				isProcessing = false;
+			}
+			else if (number < 0 || number > range) {
+				DataBot.getApi().sendMessage(message->chat->id, "Wrong range: "+ std::to_string(range));
+				isProcessing = false;
+			}
+		}
+	});
+	
+}
+
 int main() {
-	thread_pool poolOfThreads(4);
-	std::atomic<bool> NonCommandMessagesSubdiv{ false };
-	std::atomic<bool> NonCommandMessagesFibonacci{ false };
-	
+
+	std::vector<std::string> bot_commands = { "start", "find_fibonacci", "subdiv_5" };
+
+	ThreadPool poolOfThreads(2);
 
 
-	Bot bot("ENTER YOUR TOKEN");
+	TgBot::Bot bot("YOUR TOKEN ID");
 
+	bool Controller = true;
 
-	
-	int64_t ChatID;
-	
-
-	bot.getEvents().onCommand("start", [&bot, &ChatID](Message::Ptr message) {
+	bot.getEvents().onCommand("start", [&bot](TgBot::Message::Ptr message) {
 		bot.getApi().sendMessage(message->chat->id, "Hi " + message->chat->firstName);
 	});
-	bot.getEvents().onCommand("subdiv_5", [&bot, &NonCommandMessagesSubdiv, &poolOfThreads](Message::Ptr message) {
+	bot.getEvents().onCommand("subdiv_5", [&bot, &poolOfThreads, &Controller](TgBot::Message::Ptr message) {
 		bot.getApi().sendMessage(message->chat->id, "Write a number from 1 to 10000");
 
-		// Set the flag to allow processing of non-command messages
-		NonCommandMessagesSubdiv.store(true);
-
-		bot.getEvents().onNonCommandMessage([&bot, &NonCommandMessagesSubdiv, &poolOfThreads](Message::Ptr message) {
-			if (NonCommandMessagesSubdiv.load()) {
-				long long number = std::stoi(message->text);
-				if (number > 0 && number <= 10000) {
-					long long results = 0;
-					poolOfThreads.add_task(CalcSumDivisibleBy5, std::ref(number), std::ref(results));
-					poolOfThreads.wait_all();
-					bot.getApi().sendMessage(message->chat->id, "The summation of all numbers that can be divided by 5 without any remainder is " + to_string(results));
-					// Reset the flag after processing the message
-					NonCommandMessagesSubdiv.store(false);
-				}
-				else if (number < 0 || number > 10000) {
-					bot.getApi().sendMessage(message->chat->id, "Wrong range");
-					// Reset the flag after processing the message
-					NonCommandMessagesSubdiv.store(false);
-				}
-			}
-		});
+		//Call event
+		BotEvent(bot, poolOfThreads, CalcSumDivisibleBy5, 1000, Controller);
+		Controller = true;
+		
 	});
-	bot.getEvents().onCommand("find_fibonacci", [&bot, &NonCommandMessagesFibonacci, &poolOfThreads](Message::Ptr message) {
+	bot.getEvents().onCommand("find_fibonacci", [&bot, &poolOfThreads, &Controller](TgBot::Message::Ptr message) {
 		bot.getApi().sendMessage(message->chat->id, "Write a number from 1 to 90");
 
-		// Set the flag to allow processing of non-command messages
-		NonCommandMessagesFibonacci.store(true);
-
-		bot.getEvents().onNonCommandMessage([&bot, &NonCommandMessagesFibonacci , &poolOfThreads](Message::Ptr message) {
-			if (NonCommandMessagesFibonacci.load()) {
-				long long number = std::stoi(message->text);
-				if (number > 0 && number <= 90) {
-					long long results = 0;
-					poolOfThreads.add_task(CalcFibonacci, std::ref(number), std::ref(results));
-					poolOfThreads.wait_all();
-					bot.getApi().sendMessage(message->chat->id, "The " + to_string(number) + " number in Fibonacci sequence is " + to_string(results));
-					// Reset the flag after processing the message
-					NonCommandMessagesFibonacci.store(false);
-				}
-				else if (number < 0 || number > 90) {
-					bot.getApi().sendMessage(message->chat->id, "Wrong range");
-					// Reset the flag after processing the message
-					NonCommandMessagesFibonacci.store(false);
-				}
-			}
-		});
+		//Call event
+		BotEvent(bot, poolOfThreads, CalcFibonacci, 90 , Controller);
+		Controller = true;
 	});
-	
-	
-	
+
+
+
 
 
 
 	try {
 		printf("Bot username: %s\n", bot.getApi().getMe()->username.c_str());
-		TgLongPoll longPoll(bot);
+		TgBot::TgLongPoll longPoll(bot);
 		while (true) {
 			printf("Current value: %d\n", 0);
 			longPoll.start();
 
 		}
 	}
-	catch (TgException& e) {
-		printf("error: %s\n", e.what());
-	}
-	
-	poolOfThreads.wait_all();
+	catch (TgBot::TgException& e) {}
+
 
 	return 0;
 }
